bf04fbb0d5fac95ba85e84e8765fb84716d0f3ac
loicduchesne <loic402@gmail.com>
2023-01-28 20:41:05 -0500

    updated backend

--- TradingBot/app.py
+++ TradingBot/app.py
@@ -10,11 +10,11 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        stock = request.form["stock"]
+        user_input = request.form["stock"]
         response = openai.Completion.create(
-            model="text-davinci-003",
+            model="davinci:ft-personal-2023-01-29-00-06-56",
             max_tokens=100,
-            prompt=generate_prompt(stock),
+            prompt=generate_prompt(user_input),
             temperature=0.0,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -23,21 +23,9 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(Stock):
-    return """Show the market trends from the last 5 days, and decide if it is worth investing. If it is positive, it is worth investing.
-    If it is negative, it is not worth investing.
-
-
-Stock: Apple
-Response: There is an increase of 5.67 percent from the last 5 days. It could be a good idea to invest in.
-Stock: Pepsi
-Response: There is a decrease of 0.31 percent from the last 5 days. It might not be a good idea to invest in.
-Stock: Petco
-Response: There is an increase of 6.82 percent from the last 5 days. It could be a good idea to invest in.
-Stock: Swoop
-Response: There is a decrease of 1.47 percent from the last 5 days. It might not be a good idea to invest in.
-
-Stock: {}
-Response:""".format(
-        Stock.capitalize()
+def generate_prompt(user_input):
+    return """
+    Stock: {}
+    Response:""".format(
+        user_input.capitalize()
     )
