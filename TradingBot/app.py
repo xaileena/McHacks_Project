b1f63568573e88c4f87e8e69875a8bab3f7c67ae
loicduchesne <loic402@gmail.com>
2023-01-28 22:30:28 -0500

    updated import

--- TradingBot/app.py
+++ TradingBot/app.py
@@ -1,7 +1,9 @@
 import os
-import data_analysis
+
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+
+from TradingBot import data_analysis
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
